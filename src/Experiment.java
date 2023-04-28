d215ba7d71d96e741014d4f0a4cd4bb55f06a38b
George <108883931+GeorgePrimentas@users.noreply.github.com>
2023-04-28 11:17:08 +0100

    Initial commit

---
+++ src/Experiment.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Experiment {
+    public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+
+        System.out.print("Hello");
+        System.out.println(" & Goodbye");
+        Boolean variableName = Boolean.parseBoolean(input.next());
+        System.out.println(variableName);
+    }
+
+}
